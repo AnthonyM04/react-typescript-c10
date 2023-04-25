86d1e3b2a6053ca7039827edff0fb360dc3f5881
anthony* <72421281+AnthonyM04@users.noreply.github.com>
2023-04-25 17:05:07 -0400

    interface

---
+++ src/components/Home.tsx
@@ -0,0 +1,12 @@
+interface homeProp {
+    name: string,
+    age: number
+}
+
+export default function Home({name, age}: homeProp) {
+    return(
+        <>
+        home page {name} {age}
+        </>
+    )
+}
